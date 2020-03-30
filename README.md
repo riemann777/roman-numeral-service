3c081657672973246ce8a43d1d44b15b906fc524
Robert Dabin <robertdabin@outlook.com>
2020-03-30 18:35:32 +0100

    updates REAMDME to ref standard form & tidies tests to better document code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Roman Numerals Service
 
-Http Service to convert to/from Roman Numerals.
+Http Service to convert to/from Roman Numerals in [standard form](https://en.wikipedia.org/wiki/Roman_numerals#%22Standard%22_forms).
 
 build: `npm run build`
 
--- src/RomanNumeralService/RomanNumeralService.spec.ts
+++ src/RomanNumeralService/RomanNumeralService.spec.ts
@@ -3,51 +3,83 @@ import { RomanNumeralSymbol as Roman } from "./RomanNumeralSymbol";
 
 describe("RomanNumeralService", () => {
 
-    test("when asked to convert roman to arabic", () => {
+    describe("when asked to convert roman to arabic", () => {
 
-        expect(convertRomanToArabic([ Roman.I ])).toBe(1);
-        expect(convertRomanToArabic([ Roman.V ])).toBe(5);
-        expect(convertRomanToArabic([ Roman.I, Roman.I ])).toBe(2);
-        expect(convertRomanToArabic([ Roman.V, Roman.I ])).toBe(6);
-        expect(convertRomanToArabic([ Roman.I, Roman.V ])).toBe(4);
-        expect(convertRomanToArabic([
-            Roman.M, Roman.M, Roman.M, Roman.C, Roman.M, Roman.X, Roman.C, Roman.I, Roman.X
-        ])).toBe(3999);
-        expect(convertRomanToArabic([
-            Roman.M,
-            Roman.M,
-            Roman.M,
-            Roman.D,
-            Roman.C,
-            Roman.C,
-            Roman.C,
-            Roman.L,
-            Roman.X,
-            Roman.X,
-            Roman.X,
-            Roman.V,
-            Roman.I,
-            Roman.I,
-            Roman.I
-        ])).toBe(3888);
-        expect(convertRomanToArabic([ Roman.X, Roman.C, Roman.I, Roman.V ])).toBe(94);
-        expect(convertRomanToArabic([
-            Roman.M, Roman.M, Roman.M, Roman.X, Roman.L, Roman.I, Roman.V
-        ])).toBe(3044);
-        expect(convertRomanToArabic([
-            Roman.M, Roman.M, Roman.C, Roman.C, Roman.C, Roman.L, Roman.X, Roman.V
-        ])).toBe(2365);
-        expect(convertRomanToArabic([
-            Roman.M, Roman.C, Roman.M, Roman.X, Roman.L, Roman.I, Roman.V
-        ])).toBe(1944);
-        expect(convertRomanToArabic([ Roman.I, Roman.X ])).toBe(9);
-        expect(convertRomanToArabic([ Roman.C, Roman.D ])).toBe(400);
+        test("it converts base symbols", () => {
+
+            expect(convertRomanToArabic([Roman.I])).toBe(1);
+            expect(convertRomanToArabic([Roman.V])).toBe(5);
+            expect(convertRomanToArabic([Roman.X])).toBe(10);
+            expect(convertRomanToArabic([Roman.L])).toBe(50);
+            expect(convertRomanToArabic([Roman.C])).toBe(100);
+            expect(convertRomanToArabic([Roman.D])).toBe(500);
+            expect(convertRomanToArabic([Roman.M])).toBe(1000);
+
+        });
+
+        test("it converts simple additive symbol combinations", () => {
+
+            expect(convertRomanToArabic([Roman.I, Roman.I])).toBe(2);
+            expect(convertRomanToArabic([Roman.V, Roman.I])).toBe(6);
+            expect(convertRomanToArabic([
+                Roman.M,
+                Roman.M,
+                Roman.M,
+                Roman.D,
+                Roman.C,
+                Roman.C,
+                Roman.C,
+                Roman.L,
+                Roman.X,
+                Roman.X,
+                Roman.X,
+                Roman.V,
+                Roman.I,
+                Roman.I,
+                Roman.I
+            ])).toBe(3888);
+            expect(convertRomanToArabic([
+                Roman.M, Roman.M, Roman.C, Roman.C, Roman.C, Roman.L, Roman.X, Roman.V
+            ])).toBe(2365);
+
+        });
+
+        test("it converts simple subractive symbol combinations", () => {
+
+            expect(convertRomanToArabic([Roman.I, Roman.V])).toBe(4);
+            expect(convertRomanToArabic([Roman.I, Roman.X])).toBe(9);
+            expect(convertRomanToArabic([Roman.C, Roman.D])).toBe(400);
+
+        });
+
+        test("it converts complex symbol combinations", () => {
+
+            expect(convertRomanToArabic([
+                Roman.M, Roman.M, Roman.M, Roman.C, Roman.M, Roman.X, Roman.C, Roman.I, Roman.X
+            ])).toBe(3999);
+            expect(convertRomanToArabic([ Roman.X, Roman.C, Roman.I, Roman.V ])).toBe(94);
+
+            expect(convertRomanToArabic([
+                Roman.M, Roman.M, Roman.M, Roman.X, Roman.L, Roman.I, Roman.V
+            ])).toBe(3044);
+
+
+            expect(convertRomanToArabic([
+                Roman.M, Roman.C, Roman.M, Roman.X, Roman.L, Roman.I, Roman.V
+            ])).toBe(1944);
+
+        });
 
     });
 
-    test('when asked to convert arabic to roman', () => {
 
-        expect(convertArabicToRoman(1)).toStrictEqual([ Roman.I ]);
+    describe("when asked to convert arabic to roman", () => {
+
+        test("it converts arabic to roman", () => {
+
+            expect(convertArabicToRoman(1)).toStrictEqual([ Roman.I ]);
+
+        });
 
     });
 
