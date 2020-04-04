ca805f6ce4ebaaa359c9defca8c5efc42bd952c0
Robert Dabin <robertdabin@outlook.com>
2020-04-04 23:58:11 +0100

    refactors - pulls out conditionals in convertForArabicAtom to descriptively named functions

--- src/RomanNumeralService/RomanNumeralService.ts
+++ src/RomanNumeralService/RomanNumeralService.ts
@@ -9,17 +9,6 @@ export const convertRomanToArabic = (numeralSymbols: Roman[]) : number =>
             : arabicTotal + arabicValue
         , 0, numeralSymbols);
 
-export const shouldSubtract = (a: number, b: number): boolean =>
-    (getMagnitude(a) < getMagnitude(b * 2));
-
-export const getMagnitude = (n: number) => {
-
-    const order = Math.floor(Math.log(n) / Math.LN10 + 0.000000001);
-
-    return Math.pow(10, order).toString().length - 1;
-};
-
-
 export const convertArabicToRoman = (arabic: number) => {
 
     const arabicAtoms: number[] = [Roman.M, Roman.D, Roman.C, Roman.L, Roman.X, Roman.V, Roman.I];
@@ -31,11 +20,28 @@ export const convertArabicToRoman = (arabic: number) => {
     }, [], arabicAtoms);
 };
 
+
 const convertForArabicAtom = (arabic: number, arabicAtom: Roman, conversions: Roman[] = []): any => {
 
-    return (arabic - arabicAtom) < -1 || (arabicAtom === Roman.I && (arabic - arabicAtom) < 0)
+    return isFullyReducedByAtom(arabic, arabicAtom)
         ? conversions
-        : (arabicAtom !== Roman.I && (arabic - arabicAtom) === - 1)
+        : subtractiveNotationRequired(arabic, arabicAtom)
             ? convertForArabicAtom(arabic - arabicAtom, arabicAtom, conversions.concat(arabicAtom / arabicAtom).concat(arabicAtom))
             : convertForArabicAtom(arabic - arabicAtom, arabicAtom, conversions.concat(arabicAtom))
 };
+
+const shouldSubtract = (a: number, b: number): boolean =>
+    (getMagnitude(a) < getMagnitude(b * 2));
+
+const getMagnitude = (n: number) => {
+
+    const order = Math.floor(Math.log(n) / Math.LN10 + 0.000000001);
+
+    return Math.pow(10, order).toString().length - 1;
+};
+
+const isFullyReducedByAtom = (arabic: number, atom: number): boolean =>
+    ((arabic - atom) < -1 || (atom === Roman.I && (arabic - atom) < 0));
+
+const subtractiveNotationRequired = (arabic: number, atom: number): boolean =>
+    ((atom !== Roman.I) && (arabic - atom) === -1);
